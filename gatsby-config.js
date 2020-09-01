00ae984c2016d9263e01c07864927257d417c536
Mac <mac@presencompany.co>
2020-09-01 22:55:34 +1000

    Use node-sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,14 @@ module.exports = {
 			},
 		},
 		{
+			resolve: 'gatsby-plugin-sass',
+			options: {
+				cssLoaderOptions: {
+					camelCase: false,
+				},
+			},
+		},
+		{
 			resolve: 'gatsby-source-datocms',
 			options: {
 				apiToken: process.env.DATO_API_TOKEN,
@@ -33,11 +41,8 @@ module.exports = {
 				background_color: '#663399',
 				theme_color: '#663399',
 				display: 'minimal-ui',
-				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+				icon: `${__dirname}/src/images/gatsby-icon.png`, // This path is relative to the root of the site.
 			},
 		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
 	],
 };
