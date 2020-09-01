a055d095cef0433f810b6dda953d7067a7cffcdc
Mac <mac@presencompany.co>
2020-09-01 21:53:41 +1000

    Connect to headless cms backend

---
+++ dato.config.js
@@ -0,0 +1,14 @@
+const https = require('https');
+const fs = require('fs');
+
+
+/**
+ * Dato CMS yarn dump command
+ * 
+ * 
+ */
+
+module.exports = (async (dato, root, i18n) => {
+
+	return true;
+});
